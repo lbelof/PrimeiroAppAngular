9893320bb8a1d6f8cb22e94c374b54b172ba54c4
Leandro Belo Fernandes <leandro.belo@castinformatica.onmicrosoft.com>
2020-02-18 22:40:47 -0300

    carrinho de compras

--- src/app/restaurant-detail/reviews/reviews.component.ts
+++ src/app/restaurant-detail/reviews/reviews.component.ts
@@ -1,11 +1,23 @@
 import { Component, OnInit } from "@angular/core";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { Observable } from "rxjs/Observable";
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "mt-reviews",
   templateUrl: "./reviews.component.html"
 })
 export class ReviewsComponent implements OnInit {
-  constructor() {}
+  reviews: Observable<any>;
 
-  ngOnInit() {}
+  constructor(
+    private restaurantService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.reviews = this.restaurantService.reviewsOfRestaurant(
+      this.route.parent.snapshot.params["id"]
+    );
+  }
 }
